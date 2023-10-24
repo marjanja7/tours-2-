738bd1228809bedcafc7e52a195b065321869e67
Marina Ivanova <Marjanja7@mail.ru>
2023-10-24 20:25:56 +0300

    добавила выпадающее окно для вбранного тура

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { container } = require("postcss")
 import { format, differenceInDays } from "date-fns"
 import { ru } from "date-fns/locale"
 
+let tours = []
 
 const cards = document.getElementById("cards")
 
@@ -14,6 +15,13 @@ async function loadTours() {
     
     return data
 }
+
+async function init() {
+    tours = await loadTours()
+    renderTours(tours)
+}
+init()
+
 
 function renderTours(tours) {
     cards.innerHTML = ""
@@ -28,24 +36,14 @@ function renderTours(tours) {
                 <div class="p-5">
                     <div>
                         <a href="#">
-                            <p class="font-semibold mt-3 text-lg text-rose-600">${
-                                tour.country
-                            }</p>
-                            <p class='font-semibold mt-3 text-Sm text-rose-400'>${
-                                tour.city
-                            }</p>
+                            <p class="font-semibold mt-3 text-lg text-rose-600">${tour.country}</p>
+                            <p class='font-semibold mt-3 text-Sm text-rose-400'>${tour.city !== null ? tour.city : ""}</p>
                             <p class="text-grey-400 mt-3">${tour.hotelName}</p>
                         </a>
                         <p class="text-teal-500 font-medium text-sm pt-2">
-                            <a href="#">${format(
-                                new Date(tour.startTime),
-                                "dd MMMM yyyy",
-                                { locale: ru }
-                            )}-${format(
-            new Date(tour.endTime),
-            "dd MMMM yyyy",
-            { locale: ru }
-        )}</a>
+                            <a href="#">${format(new Date(tour.startTime),"dd MMMM yyyy", { locale: ru }
+                                )}-${format(new Date(tour.endTime),"dd MMMM yyyy",{ locale: ru }
+                           )}</a>
                         </p>
                     </div>
                     <div class="mt-3 text-grey-400 text-sm flex items-center gap-1">
@@ -53,24 +51,119 @@ function renderTours(tours) {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
                         <span class="ml-1">Длительность:</span>
-                        <span class="text-fblack font-medium" >${duration
-                        }</span>
+                        <span class="text-fblack font-medium" >${duration}</span>
                     </div>
 
-                    <p class="py-2 text-2xl text-rose-600 font-bold">${
-                        tour.price
-                    }</p>
-                    <button class="btn-primary w-full">Подробнее</button>
+                    <p class="py-2 text-2xl text-rose-600 font-bold">${tour.price}</p>
+                    <div class="buttons flex">
+                        <button class="btn-primary w-full ">Подробнее</button>
+                        <button id="register-btn-${tour.id}" class="btn-primary w-full">Забронировать</button>
+                    </div>
                 </div>
                 
             </div>
             </div>
         `
+        })
+        tours.forEach((tour) => {
+            const reservationBtn = document.getElementById(`register-btn-${tour.id}`)
+            reservationBtn.addEventListener("click", () => {
+                openContainerRegister (tour.id)
+
+            
+        })
     })
 }
 
-async function init() {
-    const tours = await loadTours()
-    renderTours(tours)
+
+
+const containerRegister = document.getElementById ('container-register')
+const openContainerBtn = document.getElementById (`register-btn-${tours.id}`)
+
+const sendBtn = document.getElementById('send-btn')
+
+
+//reservationBtn.addEventListener('click', openContainerRegister )
+//closeRegisterBtn.addEventListener('click', closeContainerRegister)  
+
+sendBtn.addEventListener("click", (event) => sendTour(event))
+
+let currentId
+
+function openContainerRegister (id) {
+    currentId = id
+    containerRegister.style.display = 'flex'
+    const currentTour = tours.find((u) => {
+        return u.id === id
+    })
+
+    document.getElementById("tour-info").innerHTML = `
+                <div class="flex justify-between"
+                    <img class="h-30 w-full" src="${currentTour.image}" alt="" />
+                    <div class="block color-"
+                        <p class="font-semibold mt-3 text-lg text-rose-600">${currentTour.country}
+                        <div>
+                            <p class='font-semibold mt-3 text-Sm text-rose-400'>${currentTour.city !== null ? currentTour.city : ""}</p>
+                      
+                            <p class="text-grey-400 mt-3">${currentTour.hotelName}</p>
+                        </div>
+                        <p class="text-teal-500 font-medium text-sm pt-2">
+                            <a href="#">${format(new Date(currentTour.startTime),"dd MMMM yyyy", { locale: ru }
+                                )}-${format(new Date(currentTour.endTime),"dd MMMM yyyy",{ locale: ru }
+                           )}</a>
+                        </p>
+                        <p class="py-2 text-2xl text-rose-600 font-bold">${currentTour.price}</p>
+
+
+                    </div> 
+                </div>
+    `
+
 }
-init()
+
+const closeRegisterBtn = document.getElementById ('close-register-btn')
+closeRegisterBtn.addEventListener('click', closeContainerRegister)
+
+function closeContainerRegister () {
+    containerRegister.style.display = 'none'
+  }
+
+  async function sendTour (t) {
+    const form = document.getElementById("form").value
+    const userName = document.getElementById("name").value
+    const userPhone = document.getElementById("phone").value
+    const userEmail = document.getElementById("email").value
+    const userComment = document.getElementById("comment").value
+
+    let userData = {
+        customerName: userName,
+        phone: userPhone,
+        email: userEmail,
+        comment: userComment
+
+    }
+
+    const tourId = currentId
+
+    const url = `https://www.bit-by-bit.ru/api/student-projects/tours/${tourId}`
+
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(userData)
+    })
+    if (response.ok) {
+        alert("Ваше обращение зарегистрировано")
+        closeContainerRegister()
+        let result = await response.json()
+        return result
+    }else {
+        alert("Повторите еще раз! Произошла ошибка")
+    }
+  }
+ 
+function clearContainer () {
+    document.getElementById('name').value = ""
+    document.getElementById('phone').value = ""
+    document.getElementById('email').value = ""
+    document.getElementById('comment').value = ""
+}
